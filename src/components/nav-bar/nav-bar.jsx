67ec4d691f9bb1907c4e797b44e7fd7e4952f476
Nhi Nguyen <lamnhi.ngngoc@gmail.com>
2025-11-03 14:05:13 +0200

    Add nav bar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,12 +1,23 @@
 import './nav-bar.css';
+import { Icon } from '@iconify/react';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h2 className="logo">MyApp</h2>
+      <img src="src/assets/lab-logo.png" alt="Logo" />
       <ul className="nav-links">
-        <li>Home</li>
-        <li>About</li>
+        <li>
+          <Icon icon="material-symbols:home-rounded" width="28" height="28" />
+          <span>Home</span>
+        </li>
+        <li>
+          <Icon icon="fa6-solid:headphones" width="28" height="28" />
+          <span>Listening</span>
+        </li>
+        <li>
+          <Icon icon="famicons:game-controller" width="28" height="28" />
+          <span>Game</span>
+        </li>
       </ul>
     </nav>
   );
