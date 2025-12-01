2509344e5371a7f43083b4ab32c937f947dd1815
truonghoangthong <126312478+truonghoangthong@users.noreply.github.com>
2025-12-01 12:43:15 +0200

    added component Sanapyramidi Item

---
+++ src/components/SanapyramidiItem/SanapyramidiItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./SanapyramidiItem.css";
+import { useState, useEffect, useRef } from "react";
+
+const SanapyramidiItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SanapyramidiItem;
